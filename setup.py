5c1853e04cc384d0fa494521a22629a4fbb320b1
Michael Samsom <mike.samsom@asica.ca>
2025-07-31 12:14:27 -0600

    removing upper limit breaks C bindings

--- setup.py
+++ setup.py
@@ -327,7 +327,7 @@ docs_testenv = core_testenv + distributed_env + visdom_req + [
 #
 # see https://github.com/piskvorky/gensim/pull/3535
 #
-NUMPY_STR = 'numpy >= 1.18.5'
+NUMPY_STR = 'numpy >= 1.18.5, < 2.0'
 
 install_requires = [
     NUMPY_STR,
